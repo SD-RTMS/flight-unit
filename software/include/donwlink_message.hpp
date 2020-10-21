459c29d23a380604c864afd880da226153ef3c7d
Ezra Taimuty-Loomis <etaimutyloomis@gmail.com>
2020-10-21 15:18:00 -0400

    Initial pb integration

--- software/include/donwlink_message.hpp
+++ software/include/donwlink_message.hpp
@@ -3,10 +3,12 @@
 #include <inttypes.h>
 #include <array>
 
-#include "proto_types.hpp"
+#include "messages.pb.h"
 
-#define PACKET_CHECKSUM_LENGTH_BYTES    8u
+#define PACKET_CHECKSUM_LENGTH_BYTES    2u
 #define PACKET_DATALEN_LENGTH_BYTES     2u
+#define PACKET_HEADER_LENGTH_BYTES      (PACKET_CHECKSUM_LENGTH_BYTES + PACKET_DATALEN_LENGTH_BYTES)
+#define LOCAL_BUFFER_SIZE               256u
 
 // Convenience class for encapsulation of downlink message data
 class DownlinkMessage {
@@ -16,11 +18,11 @@ public:
 
     // Construct message from serialized proto
     // Will calculate length and checksum
-    explicit DownlinkMessage(uint8_t serializedProto[]);
+    explicit DownlinkMessage(uint8_t serializedProto[], size_t length);
 
     // Construct message from a non-serialized proto
     // Will serialize and calculate message params
-    explicit DownlinkMessage(downlink_proto::SystemMetrics& metrics);
+    explicit DownlinkMessage(downlink_proto_SystemMetrics& metrics);
 
     // Verifies checksum is correct given data
     // Useful if constructed from raw packet
@@ -37,17 +39,17 @@ public:
     uint16_t packetLength();
 
     // Returns the stored checksum
-    uint64_t checksum();
+    uint16_t checksum();
 
     // Writes the packet data into `out`
     // Returns false if packet cannot fit in the buffer
     bool data(uint8_t* out, size_t bufferSize);
 
 private:
-    uint64_t _checksum;
-    uint16_t _length;
-    uint8_t _serializedProto[];
+    uint16_t _checksum;
+    uint16_t _dataLength;
+    uint8_t _serializedProto[LOCAL_BUFFER_SIZE];
 
     // Computes a checksum from _serializedProto[]
-    uint64_t _computeChecksum();
+    uint16_t _computeChecksum();
 };
